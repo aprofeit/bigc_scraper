063e5a9607c231ff4ea8d5d456f355855b3108a9
Alexander Profeit <alex@profeit.com>
2014-08-10 20:47:30 +0000

    single length

--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import log "github.com/Sirupsen/logrus"
 
+func buildUrlAtIndex(url []byte, index int, char int) {
+	url[index] = byte(char)
+
+	log.Info(string(url))
+}
+
+func findShops(url []byte) {
+	for urlIndex := 0; urlIndex < len(url); urlIndex++ {
+		for i := 48; i <= 57; i++ {
+			buildUrlAtIndex(url, urlIndex, i)
+		}
+
+		for i := 97; i <= 122; i++ {
+			buildUrlAtIndex(url, urlIndex, i)
+		}
+	}
+}
+
 func main() {
-	for i := 48; i <= 57; i++ {
-		log.Infof("%v: %v", i, string(i))
-	}
+	for shopLength := 1; shopLength <= 1; shopLength++ {
+		shopURL := make([]byte, shopLength)
 
-	for i := 97; i <= 122; i++ {
-		log.Infof("%v: %v", i, string(i))
+		findShops(shopURL)
 	}
-
-	shop := make([]byte, 2)
-	shop[0] = 49
-	log.Infof("%v", string(shop))
 }
