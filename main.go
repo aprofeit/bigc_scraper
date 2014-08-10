b4030a6290dfac53fa572e6001509c1d2383b9be
Alexander Profeit <alex@profeit.com>
2014-08-10 22:36:06 +0000

    5-7 in length 30/sec

--- main.go
+++ main.go
@@ -1,29 +1,79 @@
 package main
 
-import log "github.com/Sirupsen/logrus"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
-func buildUrlAtIndex(url []byte, index int, char int) {
-	url[index] = byte(char)
+var urls chan string
+var checked int
 
-	log.Info(string(url))
+func checkShops() {
+	for {
+		url := <-urls
+		checkShopURL(url)
+	}
 }
 
-func findShops(url []byte) {
-	for urlIndex := 0; urlIndex < len(url); urlIndex++ {
-		for i := 48; i <= 57; i++ {
-			buildUrlAtIndex(url, urlIndex, i)
-		}
+func checkShopURL(url string) {
+	resp, err := http.Get(url)
 
-		for i := 97; i <= 122; i++ {
-			buildUrlAtIndex(url, urlIndex, i)
-		}
+	checked++
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	log.Printf("[%v] %v", resp.StatusCode, url)
+}
+
+func setURLAtIndexToChar(url []byte, index int, char byte) {
+	url[index] = char
+
+	if index < len(url)-1 {
+		buildURLAtIndex(url, index+1)
+	} else {
+		shopURL := fmt.Sprintf("http://store-%v.mybigcommerce.com", string(url))
+		urls <- shopURL
+	}
+}
+
+func buildURLAtIndex(url []byte, index int) {
+	for i := 48; i <= 57; i++ {
+		setURLAtIndexToChar(url, index, byte(i))
+	}
+
+	for i := 97; i <= 122; i++ {
+		setURLAtIndexToChar(url, index, byte(i))
+	}
+}
+
+func init() {
+	urls = make(chan string, 1000)
+}
+
+func reportChecked() {
+	secondsPassed := 0
+	ticker := time.NewTicker(10 * time.Second)
+	for {
+		<-ticker.C
+		secondsPassed += 10
+		log.Printf("%v checked per second", float64(checked)/float64(secondsPassed))
 	}
 }
 
 func main() {
-	for shopLength := 1; shopLength <= 1; shopLength++ {
+	for i := 0; i < 1000; i++ {
+		go checkShops()
+	}
+
+	go reportChecked()
+
+	for shopLength := 5; shopLength <= 7; shopLength++ {
 		shopURL := make([]byte, shopLength)
 
-		findShops(shopURL)
+		buildURLAtIndex(shopURL, 0)
 	}
 }
